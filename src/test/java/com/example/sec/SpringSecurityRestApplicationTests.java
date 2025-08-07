901653fb965e2f4bc3b9ce1824fa225e3177d24c
Emanuele <egalli64@gmail.com>
2025-08-07 11:38:34 +0200

    minimal load test

---
+++ src/test/java/com/example/sec/SpringSecurityRestApplicationTests.java
@@ -0,0 +1,16 @@
+/*
+ * Spring Boot Security REST tutorial 
+ * 
+ * https://github.com/egalli64/spring-security-rest
+ */
+package com.example.sec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityRestApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
