f8f39084cfa449e5ebd1f54bd2cc655c9605fd2d
Emanuele <egalli64@gmail.com>
2025-08-09 11:27:57 +0200

    response support

---
+++ src/main/java/com/example/sec/MessageResponse.java
@@ -0,0 +1,9 @@
+/*
+ * Spring Boot Security REST tutorial 
+ * 
+ * https://github.com/egalli64/spring-security-rest
+ */
+package com.example.sec;
+
+public record MessageResponse(String message) {
+}
